bc40bbeef708805833387f489fbd80b6b2978b50
igorkll <frerther@gmail.com>
2025-05-22 14:55:39 +0300

    backup

--- EditorForm.cs
+++ EditorForm.cs
@@ -2,10 +2,14 @@ namespace WinBox_Maker
 {
     public partial class EditorForm : Form
     {
+        WinBoxProject winBoxProject;
+
         public EditorForm(string path)
         {
             InitializeComponent();
             this.Text = $"{Program.version} - {this.Text} ({Path.GetFileName(Path.GetDirectoryName(path))})";
+
+            winBoxProject = new WinBoxProject(path);
         }
 
         private void StartBuild_Click(object sender, EventArgs e)
--- WinBoxProject.cs
+++ WinBoxProject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,14 @@ namespace WinBox_Maker
         string wnbFilePath;
         string baseDirectoryPath;
         string buildDirectoryPath;
+        string resourceDirectoryPath;
 
         public WinBoxProject(string wnbFilePath)
         {
             this.wnbFilePath = wnbFilePath;
-            this.wnbFilePath = wnbFilePath;
+            baseDirectoryPath = Path.GetDirectoryName(wnbFilePath);
+            buildDirectoryPath = Path.Combine(baseDirectoryPath, "winbox_build");
+            resourceDirectoryPath = Path.Combine(baseDirectoryPath, "winbox_resource");
         }
     }
 }
