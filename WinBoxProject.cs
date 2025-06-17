18ad239ec7cd477f8df0614432555fdc01d046e7
igorkll <frerther@gmail.com>
2025-06-17 22:41:31 +0300

    Update WinBoxProject.cs

--- WinBoxProject.cs
+++ WinBoxProject.cs
@@ -333,7 +333,7 @@ namespace WinBox_Maker
                 await Program.ExecuteAsync("reg.exe", $"import reg\\hide_cursor.reg");
             }
 
-            // ------------------------------------ system setup
+            // ------------------------------------ system init
             string baseSetup = $@"@echo off
 reagentc.exe /disable
 
@@ -353,25 +353,13 @@ reg add ""HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\System""
 net user winbox /add
 net localgroup Administrators winbox /add
 
-schtasks /create /tn ""RunSetupUser"" /tr ""C:\WinboxResources\SetupUser.cmd"" /sc once /st 00:00 /ru winbox /rl highest /f
-schtasks /run /tn ""RunSetupUser""
-
-:wait
-timeout /t 1 >nul
-schtasks /query /tn ""RunSetupUser"" | find ""Running"" >nul
-if %errorlevel%==0 (
-    goto wait
-)";
+for /f ""tokens=2 delims=="" %%i in ('wmic useraccount where name^=""winbox"" get sid /format:list ^| find ""=""') do set ""SID=%%i""
+reg add ""HKEY_USERS\%SID%\Control Panel\Accessibility\StickyKeys"" /v Flags /t REG_DWORD /d 506 /f";
             if (winBoxConfig.UseOemKey == true && !winBoxConfig.OemKey.Contains("\""))
             {
                 baseSetup += $"\ncscript /B \"%windir%\\system32\\slmgr.vbs\" /ipk \"{winBoxConfig.OemKey}\"\ncscript /B \"%windir%\\system32\\slmgr.vbs\" /ato";
             }
             await File.WriteAllTextAsync(Path.Combine(WindowsScriptsPath, "SetupComplete.cmd"), baseSetup);
-
-            // ------------------------------------ user setup
-            string userSetup = $@"@echo off
-reg add ""HKEY_CURRENT_USER\Control Panel\Accessibility\StickyKeys"" /v Flags /t REG_DWORD /d 506 /f";
-            await File.WriteAllTextAsync(Path.Combine(WinboxResourcesPath, "SetupUser.cmd"), userSetup);
 
             // ------------------------------------ copy files
             string filesPath = Path.Combine(resourcesDirectoryPath, "files");
