de28587e2f8233de6e190cb2e3f13d15a6f66aa0
igorkll <frerther@gmail.com>
2025-07-03 16:43:06 +0300

    Update WinBoxProject.cs

--- WinBoxProject.cs
+++ WinBoxProject.cs
@@ -721,9 +721,9 @@ IF NOT EXIST ""C:\WinboxResources\drivers.installed"" (
                 await CopyResource("usbmmidd_v2\\install_driver.bat");
                 await CopyResource("usbmmidd_v2\\add_display.bat");
                 string regStr = $"\r\nreg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\WUDF\\Services\\usbmmIdd\\Parameters\\Monitors\" /v 0 /t REG_SZ /d \"{winBoxConfig.VirtualDisplayWidth},{winBoxConfig.VirtualDisplayHeight}\" /f";
-                string addDriver = $"\r\ncall C:\\WinboxResources\\usbmmidd_v2\\install_driver.bat";
+                string installDriver = $"\r\ncall C:\\WinboxResources\\usbmmidd_v2\\install_driver.bat";
                 string addDisplay = $"\r\ncall C:\\WinboxResources\\usbmmidd_v2\\add_display.bat";
-                baseSetup += addDriver;
+                baseSetup += installDriver;
                 baseSetup += regStr;
                 applicationScript += addDisplay;
             }
