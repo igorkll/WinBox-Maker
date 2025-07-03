697201f56257e0222ff84c8adfdf6ffdc1f81b93
igorkll <frerther@gmail.com>
2025-07-03 14:07:28 +0300

    backup

--- embedded/esp32_embedded_display/main/main.c
+++ embedded/esp32_embedded_display/main/main.c
@@ -11,9 +11,11 @@
 #define UART_NUM UART_NUM_0
 #define BUF_SIZE 1024
 
-void uart_init() {
+static hal_touchscreen_point touchInfo[TOUCHSCREEN_MAXTOUCH];
+
+static void uart_init() {
     const uart_config_t uart_config = {
-        .baud_rate = 5000000,
+        .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
@@ -23,6 +25,12 @@ void uart_init() {
     ESP_ERROR_CHECK(uart_driver_install(UART_NUM, BUF_SIZE, BUF_SIZE, 0, NULL, 0));
     ESP_ERROR_CHECK(uart_param_config(UART_NUM, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+}
+
+static void sendTouch(char prefix, hal_touchscreen_point touchPoint) {
+    char buffer[64];
+    sprintf(buffer, "%c:%i:%i\n", prefix, touchPoint.x, touchPoint.y);
+    uart_write_bytes(UART_NUM, buffer, strlen(buffer));
 }
 
 void _main() {
@@ -39,7 +47,22 @@ void _main() {
     hal_display_backlight(true);
 
     uart_init();
-    uart_write_bytes(UART_NUM, "hello", 6);
     
-
+    while (true) {
+        uint8_t touchCount = hal_touchscreen_touchCount();
+        if (touchCount > TOUCHSCREEN_MAXTOUCH) touchCount = TOUCHSCREEN_MAXTOUCH;
+        for (size_t i = 0; i < TOUCHSCREEN_MAXTOUCH; i++) {
+            hal_touchscreen_point touchPoint = {};
+            if (i < touchCount) touchPoint = hal_touchscreen_getPoint(i);
+            hal_touchscreen_point oldTouchPoint = touchInfo[i];
+            if (touchPoint.z > 0 && oldTouchPoint.z == 0) {
+                sendTouch('P', touchPoint);
+            } else if (touchPoint.z == 0 && oldTouchPoint.z > 0) {
+                sendTouch('R', oldTouchPoint);
+            } else if (touchPoint.x != oldTouchPoint.x || touchPoint.y != oldTouchPoint.y) {
+                sendTouch('D', touchPoint);
+            }
+            touchInfo[i] = touchPoint;
+        }
+    }
 }
