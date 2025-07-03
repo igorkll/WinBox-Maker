f7e554a029fef33e36cdde68aa178831f52fc7e3
igorkll <frerther@gmail.com>
2025-07-03 16:07:50 +0300

    backup

--- WinBoxProject.cs
+++ WinBoxProject.cs
@@ -718,13 +718,13 @@ IF NOT EXIST ""C:\WinboxResources\drivers.installed"" (
             if (winBoxConfig.AddVirtualDisplay == true)
             {
                 await UnpackBlob("usbmmidd_v2.zip");
-                await CopyResource("usbmmidd_v2\\custom_usbmmidd.bat");
+                await CopyResource("usbmmidd_v2\\install_driver.bat");
+                await CopyResource("usbmmidd_v2\\add_display.bat");
                 string regStr = $"\r\nreg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\WUDF\\Services\\usbmmIdd\\Parameters\\Monitors\" /v 0 /t REG_SZ /d \"{winBoxConfig.VirtualDisplayWidth},{winBoxConfig.VirtualDisplayHeight}\" /f";
-                string addDisplay = $"\r\ncall C:\\WinboxResources\\usbmmidd_v2\\custom_usbmmidd.bat";
+                string addDriver = $"\r\ncall C:\\WinboxResources\\usbmmidd_v2\\install_driver.bat";
+                string addDisplay = $"\r\ncall C:\\WinboxResources\\usbmmidd_v2\\add_display.bat";
+                baseSetup += addDriver;
                 baseSetup += regStr;
-                baseSetup += addDisplay;
-                baseSetup += regStr;
-                applicationScript += regStr;
                 applicationScript += addDisplay;
             }
 
--- resources/usbmmidd_v2/add_display.bat
+++ resources/usbmmidd_v2/add_display.bat
@@ -4,12 +4,10 @@
 @exit
 
 :AMD64
-@cmd /c deviceinstaller64.exe install usbmmidd.inf usbmmidd
 @cmd /c deviceinstaller64.exe enableidd 1
 @goto end
 
 :x86
-@cmd /c deviceinstaller.exe install usbmmidd.inf usbmmidd
 @cmd /c deviceinstaller.exe enableidd 1
 
 :end
