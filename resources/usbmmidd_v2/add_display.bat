5982a4376f095451751fe84e6ca0d93610ffff1e
igorkll <frerther@gmail.com>
2025-07-04 16:33:42 +0300

    Update add_display.bat

--- resources/usbmmidd_v2/add_display.bat
+++ resources/usbmmidd_v2/add_display.bat
@@ -5,11 +5,11 @@
 
 :AMD64
 @cmd /c deviceinstaller64.exe enableidd 1
-sc.exe create usbmmidd binPath="C:\WinboxResources\usbmmidd_v2\deviceinstaller64.exe enableidd 1"
+sc.exe create usbmmidd binPath="C:\WinboxResources\usbmmidd_v2\deviceinstaller64.exe enableidd 1" start=auto
 @goto end
 
 :x86
 @cmd /c deviceinstaller.exe enableidd 1
-sc.exe create usbmmidd binPath="C:\WinboxResources\usbmmidd_v2\deviceinstaller.exe enableidd 1"
+sc.exe create usbmmidd binPath="C:\WinboxResources\usbmmidd_v2\deviceinstaller.exe enableidd 1" start=auto
 
 :end
