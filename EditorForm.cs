199889bfad3b5801f8f444a3e01e14f2956a372a
igorkll <frerther@gmail.com>
2025-09-12 00:39:33 +0300

    Update EditorForm.cs

--- EditorForm.cs
+++ EditorForm.cs
@@ -561,6 +561,7 @@ namespace WinBox_Maker
 
             appdelay_time_value.Enabled = winBoxProject.winBoxConfig.appdelay_time == true;
             appdelay_internet_checkurl.Enabled = winBoxProject.winBoxConfig.appdelay_internet == true;
+            appdelay_internet_requestdelay.Enabled = winBoxProject.winBoxConfig.appdelay_internet == true;
 
             CustomBootLogo_UseLogoBeforeApp.CheckState = winBoxProject.winBoxConfig.CustomBootLogo_UseLogoBeforeApp == true ? CheckState.Checked : CheckState.Unchecked;
             logoBeforeApp_panel.Enabled = winBoxProject.winBoxConfig.CustomBootLogo_UseLogoBeforeApp != true;
