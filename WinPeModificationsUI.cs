2c7a26323a9111bc2c86a61c8b3271a9cf0687e0
igorkll <frerther@gmail.com>
2025-12-26 00:05:59 +0300

    added lock gui events for winPE modifications

--- WinPeModificationsUI.cs
+++ WinPeModificationsUI.cs
@@ -13,6 +13,7 @@ namespace WinBox_Maker
     public partial class WinPeModificationsUI : Form
     {
         WinPeModifications winPeModifications;
+        bool guiEventsLock = false;
 
         public WinPeModificationsUI(WinPeModifications winPeModifications, string titleSuffix)
         {
@@ -26,22 +27,30 @@ namespace WinBox_Maker
 
         void UpdateGui()
         {
+            guiEventsLock = true;
+
             applyBaseSystemBCD.Checked = winPeModifications.applyBaseSystemBCD == true;
             app_override.Checked = winPeModifications.app_override == true;
             app_tab.SelectedIndex = (int)(winPeModifications.app_override_type ?? 0);
             app_custom_cmdline.Text = winPeModifications.app_custom_cmdline ?? "";
 
             app_tab.Enabled = winPeModifications.app_override == true;
+
+            guiEventsLock = false;
         }
 
         private void applyBaseSystemBCD_CheckedChanged(object sender, EventArgs e)
         {
+            if (guiEventsLock) return;
+
             winPeModifications.applyBaseSystemBCD = applyBaseSystemBCD.Checked;
             Program.winBoxProject.SaveConfig();
         }
 
         private void app_override_CheckedChanged(object sender, EventArgs e)
         {
+            if (guiEventsLock) return;
+
             winPeModifications.app_override = app_override.Checked;
             Program.winBoxProject.SaveConfig();
             UpdateGui();
@@ -49,12 +58,16 @@ namespace WinBox_Maker
 
         private void app_custom_cmdline_TextChanged(object sender, EventArgs e)
         {
+            if (guiEventsLock) return;
+
             winPeModifications.app_custom_cmdline = app_custom_cmdline.Text;
             Program.winBoxProject.SaveConfig();
         }
 
         private void app_tab_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (guiEventsLock) return;
+
             winPeModifications.app_override_type = (AppOverrideType)app_tab.SelectedIndex;
             Program.winBoxProject.SaveConfig();
         }
