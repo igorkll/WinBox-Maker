b7a0c10b89d84cf14f815cb2a05986c5a8bb3ea8
igorkll <frerther@gmail.com>
2025-12-30 19:22:56 +0300

    Update Menu.hpp

--- other_sources/WinboxMakerRecovery/Menu.hpp
+++ other_sources/WinboxMakerRecovery/Menu.hpp
@@ -1,4 +1,15 @@
 #pragma once
 #include <windows.h>
+#include <windowsx.h>
+#include <string>
+#include <vector>
+#include <fstream>
+
+class Menu_menu
+{
+public:
+    std::vector<std::string> menuEntries;
+    int selected = 0;
+};
 
 void Menu_start(HINSTANCE hInstance);
