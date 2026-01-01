f4f98f6165faee84b4b3f931865fdfe03146d48e
igorkll <frerther@gmail.com>
2026-01-01 13:21:01 +0300

    increment version number

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ namespace WinBox_Maker
 
         public const int version_major = 1;
         public const int version_minor = 8;
-        public const int version_patch = 0;
+        public const int version_patch = 1;
         public static string version_str = $"{version_major}.{version_minor}.{version_patch}";
         public static int version_num = (version_major * 10000) + (version_minor * 100) + version_patch;
 
