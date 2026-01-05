9e428f09a1e1ade05e4e85cab1419d02d7c246d4
igorkll <frerther@gmail.com>
2026-01-05 11:56:45 +0300

    Update Main.cpp

--- other_sources/WinboxMakerRecovery/Main.cpp
+++ other_sources/WinboxMakerRecovery/Main.cpp
@@ -34,18 +34,30 @@ static void entry_reboot_to_system(void* _) {
     PostQuitMessage(0);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
-    loadConsts();
+static void entry_system_info(void* _) {
+    Menu_select(&mainMenu);
+}
 
-    if (!inputData.contains("allowMenu")) return 0;
+static void loadRecoveryMenu(HINSTANCE hInstance) {
+    if (!inputData.value("allowMenu", false)) return;
 
-    if (inputData.contains("allowFactoryReset")) {
+    if (inputData.value("allowFactoryReset", false)) {
         mainMenu.addMenuEntry_noNoNoYesNo_callback("Factory reset", entry_factory_reset);
+    }
+
+    if (inputData.value("textOnInfoPage_en", false)) {
+        mainMenu.addMenuEntry_noNoNoYesNo_callback("System info", entry_system_info);
     }
 
     mainMenu.addMenuEntry_callback("Reboot to the system now", entry_reboot_to_system);
 
     Menu_select(&mainMenu);
     Menu_start(hInstance);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
+    loadConsts();
+
+    loadRecoveryMenu(hInstance);
     return 0;
 }
