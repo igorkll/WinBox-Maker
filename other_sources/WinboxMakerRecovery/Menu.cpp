a64b57e169fa141fef156349a4d0b9ba6d1d3c6f
igorkll <frerther@gmail.com>
2026-01-05 12:15:49 +0300

    Update Menu.cpp

--- other_sources/WinboxMakerRecovery/Menu.cpp
+++ other_sources/WinboxMakerRecovery/Menu.cpp
@@ -13,9 +13,11 @@ using json = nlohmann::json;
 static COLORREF color_bg = RGB(0, 0, 0);
 static COLORREF color_title = RGB(255, 0, 0);
 static COLORREF color_text = RGB(255, 255, 255);
+static COLORREF color_textShadow = RGB(64, 64, 64);
 static COLORREF color_selectedText = RGB(255, 255, 0);
 static std::string title_text = "Winbox maker recovery";
 static int lineHeight = 100;
+static int textShadowWidth = 3;
 static int screenWidth;
 static int screenHeight;
 static std::string sysDrive;
@@ -43,14 +45,16 @@ static HFONT titleFont;
 static HFONT menuFont;
 static HBITMAP menuLogo;
 
+static HFONT createMenuFont(int cHeight) {
+    return CreateFontA(cHeight, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
+        DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+        DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Arial");
+}
+
 static void initStaticObjects() {
     backgroundBrush = CreateSolidBrush(color_bg);
-    titleFont = CreateFont(lineHeight * 0.9, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
-        DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
-        DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial");
-    menuFont = CreateFont(lineHeight * 0.6, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
-        DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
-        DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial");
+    titleFont = createMenuFont(lineHeight * 0.9);
+    menuFont = createMenuFont(lineHeight * 0.6);
     menuLogo = (HBITMAP)LoadImageA(nullptr, (sysDrive + "\\WinboxMakerRecovery\\logo.bmp").c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 }
 
@@ -60,15 +64,27 @@ static Menu_menu* menu;
 
 // ------------------------------------- code
 
-static void drawCenterizedText(HDC hdc, int y, const std::string& text) {
+static void drawCenterizedText(HDC hdc, int y, const std::string& text, int xOffset = 0) {
     RECT rect;
     GetClientRect(WindowFromDC(hdc), &rect);
     rect.top = y;
     rect.bottom = y + lineHeight;
-    rect.left = 0;
-    rect.right = rect.right;
+    rect.left = xOffset;
+    rect.right = xOffset + rect.right;
 
     DrawTextA(hdc, text.c_str(), -1, &rect, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
+}
+
+static void drawCenterizedTextWithShadow(HDC hdc, int y, const std::string& text, COLORREF color) {
+    SetTextColor(hdc, color_textShadow);
+    for (int ix = -textShadowWidth; ix <= textShadowWidth; ix += textShadowWidth) {
+        for (int iy = -textShadowWidth; iy <= textShadowWidth; iy += textShadowWidth) {
+            drawCenterizedText(hdc, y + iy, text, ix);
+        }
+    }
+
+    SetTextColor(hdc, color);
+    drawCenterizedText(hdc, y, text);
 }
 
 static void drawLogo(HWND hwnd, HDC hdc, HBITMAP logo) {
@@ -123,8 +139,8 @@ static void redrawMenu(HWND hwnd) {
     SelectObject(hdc, menuFont);
     int y = lineHeight;
     for (size_t i = 0; i < menu->menuEntriesNames.size(); i++) {
-        SetTextColor(hdc, i == menu->selected ? color_selectedText : color_text);
-        drawCenterizedText(hdc, y, menu->menuEntriesNames[i]);
+        drawCenterizedTextWithShadow(hdc, y, menu->menuEntriesNames[i], i == menu->selected ? color_selectedText : color_text);
+
         y += lineHeight;
     }
 
