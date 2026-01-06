89521b54659fdd71d2038c4db7fa0601647cd409
igorkll <frerther@gmail.com>
2026-01-07 01:07:23 +0300

    Update WinBoxProject.cs

--- WinBoxProject.cs
+++ WinBoxProject.cs
@@ -1966,7 +1966,7 @@ reg add ""HKLM\SOFTWARE\Microsoft\Windows Embedded\KeyboardFilter"" /v BreakoutK
             // ------------------------------------ system init
 
             string binaryKeyboardHook = "000000000000000030000000000021e000006ce000006de0000011e000006be000003b0000004400000057000000580000006400000065000000660000006700000068000000690000006a0000003c0000006b0000006c0000006d0000006e0000006f0000003d0000003e0000003f0000004000000041000000420000004300000013e0000014e0000012e00000380000005be000005ee0000037e0000038e000005ce000005fe0000063e000006ae0000066e0000069e0000032e0000067e0000065e0000068e000000000";
-            bool hideGettingReadyScreenWithOverlay = false; //ОНО БЛЯТЬ НЕ ЗАКРЫВАЕТСЯ САМО!
+            bool hideGettingReadyScreenWithOverlay = true;
             string showImageBaseCmd = $@"powershell -ExecutionPolicy Bypass -File ""C:\WinboxResources\show_image.ps1"" ";
 
             string applicationScript = $@"@echo off" + "\r\n";
